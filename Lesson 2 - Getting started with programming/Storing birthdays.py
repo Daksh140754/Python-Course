4da0923f8d339de58fd1e0b64f491870160ee3b5
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-16 10:37:53 +0530

    Initial commit

---
+++ Lesson 2 - Getting started with programming/Storing birthdays.py
@@ -0,0 +1,11 @@
+DOB_1= "December 12,2010"
+DOB_2= "August 12,2010"
+DOB_3= "September 4,2010"
+DOB_4= "july 8,2010"
+DOB_5= "june 10,2009"
+
+print("Atiksh:", DOB_1)
+print("Puneeth:",DOB_2)
+print("Rahul:",DOB_3)
+print("Aaryan:",DOB_4)
+print("Naveen:",DOB_5)
