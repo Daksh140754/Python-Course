4c4fc01e37d4bd60b7e8eea0d64bf64c1efafebc
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-17 10:19:47 +0530

    Initial commit

---
+++ Lesson 3 - DataTypes/Typecasting.py
@@ -0,0 +1,17 @@
+name="Daksh"
+age=15
+weight=55.5
+student=True
+
+print("Data type of name is:" ,type(name))
+print("Data type of age is:",type(age))
+print("Data type of weight is:",type(weight))
+print("Data type of student is:",type(student))
+
+
+
+age=str(age)
+print("Value of age:",age,type(age))
+
+weight=int(weight)
+print("value of weight:",weight,type(weight))
