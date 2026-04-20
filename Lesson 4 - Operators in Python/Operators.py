a849fdeaa40d110aeb0277baf06023c38d7dcf5f
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-20 09:46:21 +0530

    Initial commit

---
+++ Lesson 4 - Operators in Python/Operators.py
@@ -0,0 +1,11 @@
+tree1=100
+tree2=93
+tree3=96
+tree4=65
+tree5=100
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is:",sum)
+
+average=sum/5
+print("The average of all trees is:",average)
