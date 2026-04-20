8ab833c3f885efb59686b60f1e8d9350ef0b53cc
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-20 10:04:58 +0530

    Initial commit

---
+++ Lesson 4 - Operators in Python/Finding percentage.py
@@ -0,0 +1,11 @@
+print("Enter the marks of 5 subjects:")
+maths=int(input("Enter Your Marks:"))
+science=int(input("Enter Your Marks:"))
+social=int(input("Enter Your Marks:"))
+hindi=int(input("Enter Your Marks:"))
+english=int(input("Enter Your Marks:"))
+
+sum=maths+science+social+hindi+english
+percentage=sum/500*100
+print("The Sum of All subjects:",sum)
+print("The total percentage is:",percentage)
