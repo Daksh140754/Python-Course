b31412fcc8283fcfe9fcb38c7fce32bf015a40e6
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-21 10:29:56 +0530

    Initial commit

---
+++ Lesson 5 - Conditional statements in python/Summer time.py
@@ -0,0 +1,5 @@
+temp=float(input("Enter the temperature outside:"))
+if temp>20:
+    print("You should try wearing light clothes they are comfortable!")
+else:
+    print("You have to wear stiff clothes so that u dont catch cold!!")
