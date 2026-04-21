8478dfbe0bab8c1117e24ec859bce8488fc6dbe0
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-21 09:57:02 +0530

    Initial commit

---
+++ Lesson 5 - Conditional statements in python/Greater or smaller.py
@@ -0,0 +1,8 @@
+i=15
+if i<15:
+    print("i is smaller than 15")
+    print("i m in if block")
+else:
+    print("i is greater than 15")
+    print("i m in else block")
+print("i m outside if and else")
