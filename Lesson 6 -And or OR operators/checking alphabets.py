de09766bf5368206dec055916a31b5dd63e96fae
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-22 10:41:54 +0530

    Initial Commit

---
+++ Lesson 6 -And or OR operators/checking alphabets.py
@@ -0,0 +1,11 @@
+character=input("Enter your character:")
+if (character>="a" and character<="z")==(character>="A" and character<="Z"):
+    print("The character is not an alphabet")
+else:
+    print("The character is an alphabet")
+
+
+
+
+
+
