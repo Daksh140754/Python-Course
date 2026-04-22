5f9499bb19e5fc326173149e2e92c39a06010424
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-22 09:46:52 +0530

    Initial commit

---
+++ Lesson 6 -And or OR operators/AND or OR.py
@@ -0,0 +1,15 @@
+a=0
+b=2
+c=5
+if a>b and a>c:
+    print("The boolean of these expressions are false")
+else:
+    print("The boolean of these expressions are true")
+
+a=5
+b=10
+c=6
+if a>b or b>c:
+    print("The boolean value of these expressions are true")
+else:
+    print("The boolean of these expressions are false")
