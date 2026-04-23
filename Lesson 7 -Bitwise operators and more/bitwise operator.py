b15ddf8f24f8c34912aae1dbc9f43d721c2ec942
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-23 10:03:33 +0530

    Initial commit

---
+++ Lesson 7 -Bitwise operators and more/bitwise operator.py
@@ -0,0 +1,32 @@
+x=5
+if(type(x)) is int:
+    print("True")
+else:
+    print("False")
+
+x=5.5
+if (type(x)) is float:
+    print("True")
+else:
+    print("False")
+
+x="hello world"
+if(type(x)) is str:
+    print("True")
+else:
+    print("False")
+
+x=15
+y=15
+
+if x is y:
+    print("Truee")
+else:
+    print("False")
+
+x=15
+y=40
+if x is not y:
+    print("You are on the right track")
+else:
+    print("false")
