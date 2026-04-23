8855d88c7ae4d97785af311596feb97fb505ba80
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-23 11:01:22 +0530

    Initial commit

--- Lesson 7 -Bitwise operators and more/bitwise.py
+++ Lesson 7 -Bitwise operators and more/bitwise.py
@@ -11,3 +11,30 @@ print("a<<1 =",a<<1)
 print("b<<1",b<<1)
 
 
+print("ASCII value checker")
+print("="*40)
+
+char=input("Enter a single character:")
+if type(char) is str and type(char)==1:
+    print("Valid input!")
+else:
+    print("Please enter only ONE character")
+ascii_value=ord(char)
+
+print(f"ASCII value is:,{ascii_value}")
+print(f"Character:,{char}")
+
+print("Character type:",end="")
+if ascii_value>=65 and ascii_value<=98:
+    print("Uppercase letter")
+elif ascii_value>=97 and ascii_value<=122:
+    print("Lowercase letter")
+elif ascii_value>=48 and ascii_value<=57:
+    print("Digit")
+elif ascii_value==32:
+    print("Space")
+else:
+    print("It is a special character")
+
+
+
