a816189a5c018987de3a87f398fe3452b6beed2a
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-24 09:50:11 +0530

    Initial Commit

---
+++ Lesson 8 -Python challenges/Challenge.py
@@ -0,0 +1,16 @@
+x=5
+y=2
+u=6
+a=4
+b=8
+i=9
+z=((x+y)* b/a)
+print("The value of (x+y)*b/a is: ",z)
+
+
+x="Alex"
+y=10
+if x == "Alex" or x=="Steve" and y>=15:
+    print("Hello!!!")
+else:
+    print("Bye")
