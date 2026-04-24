a48d84a1f3d3303e0dc64e852488adabc44f21a4
Daksh140754 <pramodmenondaksh@gmail.com>
2026-04-24 10:44:52 +0530

    Initial commit

---
+++ Lesson 8 -Python challenges/number swap.py
@@ -0,0 +1,6 @@
+num1=(input("Enter your number:"))
+num2=(input("Enter your second number:"))
+num3=(input("Enter your third number:"))
+
+swapped_num=num1 , num2 , num3=num3,num1,num2
+print("Swapped numbers:",swapped_num )
