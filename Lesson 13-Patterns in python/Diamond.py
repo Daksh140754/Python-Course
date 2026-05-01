15a6aae40a7bfd4bdaee3065e025bb4b7ec0365a
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-01 10:14:40 +0530

    Initial commit

---
+++ Lesson 13-Patterns in python/Diamond.py
@@ -0,0 +1,28 @@
+rowsize=int(input("Enter your rowsize:"))
+if rowsize%2==0:
+    halfdiamond=int(rowsize/2)
+else:
+    halfdiamond=int(rowsize/2)+1
+space=halfdiamond
+for i in range(1 ,halfdiamond+1):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space-1
+    num=1
+    for j in range(2*i-1):
+        print(end=str(num))
+        num=num+1
+    print()
+space=1
+for i in range(1,halfdiamond):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for i in range(1,2*(halfdiamond-i)):
+        print(end=str(num))
+        num=num+1
+    print()
+
+              
+   
