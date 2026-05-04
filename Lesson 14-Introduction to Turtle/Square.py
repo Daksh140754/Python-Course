7653cc92b0ef4db2425ef7298b088c58d033940e
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-04 10:37:12 +0530

    Initial commit

---
+++ Lesson 14-Introduction to Turtle/Square.py
@@ -0,0 +1,29 @@
+#Square
+import turtle
+my_un=turtle.Screen().bgcolor("Blue")
+my_pen=turtle.Turtle
+my_un=turtle.fillcolor("Red")
+my_un=turtle.begin_fill
+for i in range(4):
+    turtle.right(90)
+    turtle.forward(100)
+my_un=turtle.end_fill
+turtle.done()
+
+#Triangle
+import turtle
+my_un=turtle.Screen().bgcolor("White")
+my_pen=turtle.Turtle
+my_un=turtle.fillcolor("Green")
+my_un=turtle.begin_fill()
+
+for i in range(3):
+    my_un=turtle.left(120)
+    my_un=turtle.forward(100)
+my_un=turtle.end_fill()
+turtle.done()
+
+
+
+
+
