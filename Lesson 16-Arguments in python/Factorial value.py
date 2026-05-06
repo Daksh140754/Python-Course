db1708c66d90d6b8b17456dc62e1445b76ea6b20
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-06 10:01:06 +0530

    Initial commit

---
+++ Lesson 16-Arguments in python/Factorial value.py
@@ -0,0 +1,14 @@
+def factorial(x):
+    """This is a recursive function"""
+
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+
+print("The factorial of 0:",factorial(0))
+print("The factorial of 2:",factorial(2))
+print("The factorial of 5:",factorial(5))
+print("The factorial of 10:",factorial(10))
+print("The factorial of 3:",factorial(3))
+print(factorial.__doc__)
