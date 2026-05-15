bdaa83db4061c573a1c4e13ed788d744657dfa1f
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-15 16:22:23 +0530

    Initial commit

---
+++ Lesson 22- Tuples in python/tuple.py
@@ -0,0 +1,20 @@
+tuplex=("tuple" , False  , 3.2 , 1)
+print(tuplex)
+
+tuplex=(4 , 5 , 6 , 2 , 8 , 9)
+print(tuplex)
+
+tuple1=(50 , 60 , 20 , 54 , 80)
+print(len(tuple1))
+
+tuple2=(56 , 80 , 40 , 80 , 65)
+print(tuple2[3:5])
+
+tuple3=(5 , 8 , 6 , 41 , 5 , 5 , 5 , 6)
+print(tuple3.count(5))
+
+tuple4=(5 , 6 , 2 , 20 , 8 , 6 , 4 , 8)
+print(tuple4[:6])
+print(tuple4[3:])
+
+
