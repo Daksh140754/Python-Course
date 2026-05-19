65fa652a27be3add0a6d613c9f2bafc79627bda6
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-19 16:23:21 +0530

    Initial commit

---
+++ Lesson 24-Sets in python/Intersection.py
@@ -0,0 +1,10 @@
+setx={"green" , "blue"}
+sety={"violet" , "blue"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection and union of two said elements are:")
+setz=setx.intersection(sety)
+set0=setx.union(sety)
+print(setz)
+print(set0)
