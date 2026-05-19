99e4cba61b11be24c5447bcf97ee10cd6091534f
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-19 16:17:54 +0530

    Initial commit

---
+++ Lesson 24-Sets in python/Operations.py
@@ -0,0 +1,17 @@
+my_set = {1 , 2 , 3}
+print(my_set)
+
+my_set={1.0 , "Hello" , (1 , 2 , 3)}
+print(my_set)
+
+my_set = {1 , 2 , 3 , 4 , 4 , 5 , 5 , 5 , 5 , 5 , 6}
+print(my_set)
+
+my_set = set([1 , 2 , 3 , 4 , 5 , 6])
+print(my_set,"\n")
+
+num_set = set([0 , 1 , 2 , 3 , 4, 4])
+print("Original Set:")
+print(num_set)
+num_set.pop()
+print("This removes the first element!")
