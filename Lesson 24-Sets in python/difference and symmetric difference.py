0a2a2db2c50d5a706c453a357f5e5f2ae10339e7
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-19 16:39:14 +0530

    Initial commit

---
+++ Lesson 24-Sets in python/difference and symmetric difference.py
@@ -0,0 +1,10 @@
+set1={1 , 5 , 6 , 7 , 10 , 12}
+set2={2 , 10 , 15 , 6 , 20 , 3}
+setd=set1.difference(set2)
+setsd=set1.symmetric_difference(set2)
+print("The difference and symmetric difference between the two sets are:")
+print(setd)
+print(setsd)
+
+
+
