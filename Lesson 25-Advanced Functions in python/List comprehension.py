528f546a8dce35216045397980b504c2fe28b6f9
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-20 17:08:23 +0530

    Initial commit

--- Lesson 25-Advanced Functions in python/List comprehension.py
+++ Lesson 25-Advanced Functions in python/List comprehension.py
@@ -1,4 +1,10 @@
-list=[1 , 2 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
-even=[(x) for x in list  if x%2==0]
-print(even)
-      
+
+user_input = int(input("Enter a number: "))
+full_list = list(range(1, user_input))
+odd_numbers = [x for x in full_list if x % 2 != 0]
+print("All numbers:", full_list)
+print("Odd numbers under your input:", odd_numbers)
+fruits = ["apple", "banana", "cherry", "mango", "strawberry"]
+capitalized_fruits = [fruit.capitalize() for fruit in fruits]
+print("Original list:", fruits)
+print("Updated list:", capitalized_fruits)
