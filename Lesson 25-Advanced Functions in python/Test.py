8763f0fcddf758481f29cef5f4c29a14a2816eed
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-21 16:46:06 +0530

    Initial commit

---
+++ Lesson 25-Advanced Functions in python/Test.py
@@ -0,0 +1,16 @@
+dict = {"name": "Ayush" , "Score" :90/100 , "name": "Ashish" , "Score" : 95/100 , "name": "Rahul" ,  "Score" : 85/100 , "name": "Mohan" , "Score" :98/100  ,"name": "Ayush" , "Score" :70/100}
+
+average = 5/420
+
+for dict in average:
+    print(average)
+
+top = dict.max()
+
+
+
+
+
+
+
+
