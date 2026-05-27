92e19cb2ba0f6fe5938f636b2c4fee1872810d57
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-27 06:39:28 +0530

    Initial commit

---
+++ Lesson 28-Inheritance in python/employee/Penguin.py
@@ -0,0 +1,30 @@
+class Bird:
+
+
+    def __init__(self):
+        print("Bird is ready!")
+
+    def whoisthis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim Faster")
+
+
+class Penguin(Bird):
+
+    def __init__(self):
+        super().__init__()
+        print("Penguin is Ready!")
+
+    def whoisthis(self):
+        print("Penguin")
+
+    def run (self):
+        print("Run Faster")
+
+
+peggy = Penguin()
+peggy.whoisthis()
+peggy.run()
+peggy.swim()
