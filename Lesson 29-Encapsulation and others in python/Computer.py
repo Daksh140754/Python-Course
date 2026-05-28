0a0b2b92980d06ced4329a2cf161f0f43e82e26f
Daksh140754 <pramodmenondaksh@gmail.com>
2026-05-28 06:32:07 +0530

    Initial commit

---
+++ Lesson 29-Encapsulation and others in python/Computer.py
@@ -0,0 +1,20 @@
+class Computer:
+    def __init__(self):
+        self.__maxprice = 900
+
+
+    def sell(self):
+        print("Selling price is {}" .format(self.__maxprice))
+
+
+    def setmaxprice(self , price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+c.__maxprice = 1000
+c.sell()
+
+c.setmaxprice(1000)
+c.sell()
