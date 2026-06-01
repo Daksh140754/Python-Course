28e77719294d5cad3d231f97ad843d1788ce1423
Daksh140754 <pramodmenondaksh@gmail.com>
2026-06-01 06:50:32 +0530

    Initial commit

--- Lesson 27-More on object oriented programming/Circle.py
+++ Lesson 27-More on object oriented programming/Circle.py
@@ -1,9 +1,14 @@
 import math
 class Circle:
 
-    def __init__(self , radius):
-        self.radius= radius
+    def __init__(self , radius ):
+        self.radius = radius
+        self.area = math.pi**2
+        
+    def Perimeter(self ,radius, perimeter):
+        self.perimeter= 2*math.pi*radius
 
-
-    def compute_area():
-        math.pi()
+my_circle=Circle(radius=10)
+print("Area of the circle:" , my_circle.area)
+print("Perimeter of the circle:" ,sel)
+print("Radius of the circle:" , my_circle.radius)
---
+++ Lesson 30-Abstraction and Polymorphism/test.py
@@ -0,0 +1,28 @@
+class Library:
+    def __init__(self , title , author , is_borrowed):
+        self.title = title
+        self.author= author
+        self.is_borrowed = is_borrowed
+    def borrow(is_borrowed):
+        True
+        print("The book was borrowed")
+
+    def return_book(self ,):
+        False
+        print("The Book was not returned")
+
+    def author_(self):
+        print("The author is HG Wells")
+
+    def title_(self):
+        print("The Time Machine")
+
+    
+
+e=Library("The Time Machine" , "HG Wells" , True)
+e.borrow()
+e.return_book()
+e.author_()
+e.title_()
+
+
