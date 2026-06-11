4824c298845c399efe2e9ec2f21002350dcaa8bc
Daksh140754 <pramodmenondaksh@gmail.com>
2026-06-11 16:48:21 +0530

    Initial commit

---
+++ Lesson-36 Introduction to events in Tkinter/prak.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title("Checking the year")
+root.geometry('960x800')
+
+entry_year = Entry(root , height = 300 , width= 360)
+label = Label(text="Enter your Year" , bgcolor= 'grey' , fgcolor = 'black')
+
+def If_leapyear():
+
+    Entry.get()
+
+
+
+
+
